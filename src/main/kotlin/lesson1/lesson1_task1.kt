db80930ce4603c84bd8c8eaf663af2a0fb343ce4
JohnnyGomezzz <seldesign@mail.ru>
2024-10-04 16:42:17 +0300

    KS_lesson1-1 variables added

---
+++ src/main/kotlin/lesson1/lesson1_task1.kt
@@ -0,0 +1,10 @@
+package org.example.lesson1
+
+fun main() {
+    val numberOfOrders = 100
+    val thankYouText = "Спасибо за покупку!"
+
+    println("Количество заказов: $numberOfOrders")
+    println(thankYouText)
+
+}
