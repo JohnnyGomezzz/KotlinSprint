4e5c62a9d0d89b545138ca66a9828add7088b23c
JohnnyGomezzz <seldesign@mail.ru>
2024-10-04 17:39:26 +0300

    KS_lesson1-3 Gagarin's flight

---
+++ src/main/kotlin/lesson1/lesson1_task3.kt
@@ -0,0 +1,27 @@
+package org.example.lesson1
+
+fun main() {
+    val year = 1961
+    var hour = 9
+    var minute = 7
+
+    print("Год полёта Юрия Гагарина в космос: ")
+    println(year)
+
+//    println("Время взлёта:")
+//    print("0")
+//    print(hour)
+//    println(" часов")
+//
+//    print("0")
+//    print(minute)
+//    println(" минут")
+
+    hour = 10
+    minute = 55
+
+    print("Время посадки: ")
+    print(hour)
+    print(":")
+    println(minute)
+}
