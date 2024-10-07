afefd43c423ffc79f5c0a36426d26e278a3cccc6
JohnnyGomezzz <seldesign@mail.ru>
2024-10-07 15:59:24 +0300

    KS-2-5-variables-added

--- src/main/kotlin/lesson2/lesson2_task5.kt
+++ src/main/kotlin/lesson2/lesson2_task5.kt
@@ -2,12 +2,15 @@ package org.example.lesson2
 
 import kotlin.math.pow
 
+const val PERCENTS = 100
+
 fun main() {
     val startSum = 70_000
+    val increasingRatio = 1
     val rate = 16.7
     val depositPeriod = 20
 
-    val totalSum = startSum * (1 + rate / 100).pow(depositPeriod)
+    val totalSum = startSum * (increasingRatio + rate / PERCENTS).pow(depositPeriod)
 
     println(String.format("%.3f", totalSum))
 
