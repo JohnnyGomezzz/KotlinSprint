02e0843c861e19bc630d0561f26ab9d1886afdc5
JohnnyGomezzz <seldesign@mail.ru>
2024-10-08 15:52:25 +0300

    KS-3-4-code-simplified

--- src/main/kotlin/lesson3/lesson3_task4.kt
+++ src/main/kotlin/lesson3/lesson3_task4.kt
@@ -1,31 +1,14 @@
 package org.example.lesson3
 
 fun main() {
-    var moveCounter = 0
+    var from = "E2"
+    var to = "E4"
+    var moveCounter = 1
 
-    class Player(name: String, color: String) {
-        val playerName = name
-        val playerColor = color
+    println("[$from-$to;$moveCounter]")
 
-        fun move(from: String, to: String) {
-            moveCounter++
-            println(
-                String.format(
-                    "%s, %s: %s-%s; %s",
-                    playerName,
-                    playerColor,
-                    from,
-                    to,
-                    moveCounter
-                )
-            )
-        }
-    }
-
-    val player1 = Player("Bob", "white")
-    val player2 = Player("Peter", "black")
-
-    player1.move("E2", "E4")
-    player2.move("E7", "E5")
-    player1.move("D2", "D3")
+    moveCounter++
+    from = "D2"
+    to = "D3"
+    println("[$from-$to;$moveCounter]")
 }
