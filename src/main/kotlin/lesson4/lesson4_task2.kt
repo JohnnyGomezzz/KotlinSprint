11a520c4f43eddf945720bba2a908453a0808efc
JohnnyGomezzz <seldesign@mail.ru>
2024-10-09 18:26:58 +0300

    KS-4-2-check-const-added

--- src/main/kotlin/lesson4/lesson4_task2.kt
+++ src/main/kotlin/lesson4/lesson4_task2.kt
@@ -1,17 +1,21 @@
 package org.example.lesson4
 
-fun main() {
-    val minWeight = 35
-    val maxWeight = 100
-    val maxVolume = 100
+const val MIN_WEIGHT = 35
+const val MAX_WEIGHT = 100
+const val MAX_VOLUME = 100
 
+fun main() {
     var cargoWeight = 20
     var cargoVolume = 80
-    println("Груз с весом $cargoWeight кг и объемом $cargoVolume л соответствует категории 'Average': " +
-            "${minWeight < cargoWeight && cargoWeight<= maxWeight && cargoVolume < maxVolume}")
+    println(
+        "Груз с весом $cargoWeight кг и объемом $cargoVolume л соответствует категории 'Average': " +
+                "${MIN_WEIGHT < cargoWeight && cargoWeight <= MAX_WEIGHT && cargoVolume < MAX_VOLUME}"
+    )
 
     cargoWeight = 50
     cargoVolume = 100
-    println("Груз с весом $cargoWeight кг и объемом $cargoVolume л соответствует категории 'Average': " +
-            "${minWeight < cargoWeight && cargoWeight<= maxWeight && cargoVolume < maxVolume}")
+    println(
+        "Груз с весом $cargoWeight кг и объемом $cargoVolume л соответствует категории 'Average': " +
+                "${MIN_WEIGHT < cargoWeight && cargoWeight <= MAX_WEIGHT && cargoVolume < MAX_VOLUME}"
+    )
 }
