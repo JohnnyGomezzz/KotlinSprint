0746cfa721edc53d21f2fedc3d541049e1bb941e
JohnnyGomezzz <seldesign@mail.ru>
2024-10-20 08:52:43 +0300

    KS-9-4 const added

--- src/main/kotlin/lesson9/lesson9_task4.kt
+++ src/main/kotlin/lesson9/lesson9_task4.kt
@@ -1,7 +1,9 @@
 package org.example.lesson9
 
+const val NUM_OF_INGREDIENTS = 5
+
 fun main() {
-    print("Перечислите 5 необходимых ингредиентов через запятую (с пробелом): ")
+    print("Перечислите $NUM_OF_INGREDIENTS необходимых ингредиентов через запятую (с пробелом): ")
     val userIngredients = readln().split(", ")
 
     val sortedIngredients = userIngredients.sorted()
