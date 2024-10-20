b1c7d42ea2b61115dda954bfa1ad26b0bf7a65b2
JohnnyGomezzz <seldesign@mail.ru>
2024-10-20 11:04:42 +0300

    KS-10-1 dice game completed

---
+++ src/main/kotlin/lesson10/lesson10_task1.kt
@@ -0,0 +1,41 @@
+package org.example.lesson10
+
+fun main() {
+    var currentRound = 1
+    var player1Points = 0
+    var player2Points = 0
+
+    print("Введите имя, Игрок 1: ")
+    val player1Name = readln()
+    print("Введите имя, Игрок 2: ")
+    val player2Name = readln()
+    print("Введите количество раундов: ")
+    val totalRounds = readln().toInt()
+
+    while (currentRound <= totalRounds) {
+        player1Points += turnOf(player1Name)
+        player2Points += turnOf(player2Name)
+
+        currentRound++
+    }
+    val winner = if (player1Points > player2Points) "Победил $player1Name!"
+    else if (player1Points < player2Points) "Победил $player2Name!"
+    else "Ничья!"
+
+    println(
+        String.format(
+            "Результат: %s - %d очков, %s - %d очков. %s",
+            player1Name,
+            player1Points,
+            player2Name,
+            player2Points,
+            winner
+        )
+    )
+}
+
+fun turnOf(name: String): Int {
+    val points = (1..6).random()
+    println("Бросает $name!\nВыпало $points")
+    return points
+}
