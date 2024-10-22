e91400ebc53dd1cd4a77986e6a10a94618fea0b2
JohnnyGomezzz <seldesign@mail.ru>
2024-10-22 16:39:01 +0300

    KS-10-4 functions optimized

--- src/main/kotlin/lesson10/lesson10_task4.kt
+++ src/main/kotlin/lesson10/lesson10_task4.kt
@@ -4,7 +4,7 @@ fun main() {
     var countOfWins = 0
 
     while (true) {
-        countOfWins += play()
+        countOfWins += play("Человек", "Компьютер")
         println("Хотите бросить кости еще раз? Введите Да или Нет")
         if (readln().equals("да", ignoreCase = true)) continue
         else break
@@ -18,27 +18,20 @@ fun rollDice(name: String): Int {
     return points
 }
 
-fun play(): Int {
+fun play(player1Name: String, player2Name: String): Int {
     var player1Points = 0
     var player2Points = 0
-    val player1Name = "Человек"
-    val player2Name = "Компьютер"
-
-    var count = 0
 
     player1Points += rollDice(player1Name)
     player2Points += rollDice(player2Name)
 
-    if (player1Points > player2Points) {
-        println("Победил $player1Name!")
-        count = 1
-    }
-    else if (player1Points < player2Points) {
-        println("Победил $player2Name!")
-    }
-    else {
-        println("Ничья!")
-    }
+    return countOfWins(player1Points, player2Points)
+}
+
+fun countOfWins(player1Points: Int, player2Points: Int): Int {
+    var count = 0
+
+    if (player1Points > player2Points) count = 1
 
     return count
 }
