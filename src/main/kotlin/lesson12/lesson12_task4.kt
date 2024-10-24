c9ed47a122368770dd8278470199552676f9f6ec
JohnnyGomezzz <seldesign@mail.ru>
2024-10-24 21:11:43 +0300

    KS-12-4 const added

--- src/main/kotlin/lesson12/lesson12_task4.kt
+++ src/main/kotlin/lesson12/lesson12_task4.kt
@@ -1,13 +1,15 @@
 package org.example.lesson12
 
+const val CELSIUS_KELVIN = 273
+
 fun main() {
     val tuesdayWeather = KelvinWeather2(290, 287, false)
     val saturdayWeather = KelvinWeather2(295, 289, true)
 }
 
 class KelvinWeather2(_dayTemperature: Int, _nightTemperature: Int, _isRainfall: Boolean) {
-    val dayTemperature = _dayTemperature - 273
-    val nightTemperature = _nightTemperature - 273
+    val dayTemperature = _dayTemperature - CELSIUS_KELVIN
+    val nightTemperature = _nightTemperature - CELSIUS_KELVIN
     val isRainfall: Boolean = _isRainfall
 
     init {
