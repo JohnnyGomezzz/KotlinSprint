230b47af2f5986921a22620254c1c2cc8110057d
JohnnyGomezzz <seldesign@mail.ru>
2024-10-24 16:16:46 +0300

    KS-12-2 weather app with primary constructor done

---
+++ src/main/kotlin/lesson12/lesson12_task2.kt
@@ -0,0 +1,31 @@
+package org.example.lesson12
+
+fun main() {
+    val tuesdayWeather = Weather(
+        dayTemperature = 25,
+        nightTemperature = 19,
+        isRainfall = true,
+    )
+    val saturdayWeather = Weather(
+        dayTemperature = 22,
+        nightTemperature = 16,
+        isRainfall = false,
+    )
+
+    tuesdayWeather.showWeather()
+    saturdayWeather.showWeather()
+}
+
+class Weather(
+    var dayTemperature: Int,
+    var nightTemperature: Int,
+    var isRainfall: Boolean,
+) {
+    fun showWeather() {
+        println(
+            "температура днём: $dayTemperature\n" +
+                    "температура ночью: $nightTemperature\n" +
+                    "наличие осадков: $isRainfall\n"
+        )
+    }
+}
