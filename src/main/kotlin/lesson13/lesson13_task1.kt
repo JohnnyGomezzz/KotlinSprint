7a0c8d74796e6f74804721c74167cf4ce1a49ee6
JohnnyGomezzz <seldesign@mail.ru>
2024-10-25 19:50:13 +0300

    KS-13-1 class phonebook created

---
+++ src/main/kotlin/lesson13/lesson13_task1.kt
@@ -0,0 +1,15 @@
+package org.example.lesson13
+
+fun main() {
+
+}
+
+class PhoneBook(
+    _name: String,
+    _phoneNumber: Long,
+    _company: String?,
+) {
+    val name = _name
+    val phoneNumber = _phoneNumber
+    val company = _company
+}
