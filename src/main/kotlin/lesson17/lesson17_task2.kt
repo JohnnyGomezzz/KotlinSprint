0fcb5b3a92c40296570b3f9dfdf5ab85ee3d59f2
JohnnyGomezzz <seldesign@mail.ru>
2024-10-31 10:16:37 +0300

    KS-17-2 ship created

---
+++ src/main/kotlin/lesson17/lesson17_task2.kt
@@ -0,0 +1,23 @@
+package org.example.lesson17
+
+fun main() {
+    val ship = Ship("Мария", 30, "Санкт-Петербург")
+
+    ship.name = "Виктория"
+    println(ship.name)
+}
+
+class Ship(
+    _name: String,
+    val averageSpeed: Int,
+    val homePort: String,
+) {
+    var name = _name
+        set(value) {
+            if (value != field) {
+                println("Корабль переименовать нельзя!")
+            }
+            field = name
+        }
+
+}
